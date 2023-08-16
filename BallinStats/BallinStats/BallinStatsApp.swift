e877e5de17ebe3c9b1fd5df61ef3a7f3709e9332
Gerardo Ramos <gerx_37@hotmail.com>
2023-08-16 13:34:00 -0600

    Created project structure

---
+++ BallinStats/BallinStats/BallinStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BallinStatsApp.swift
+//  BallinStats
+//
+//  Created by Gerardo Leal on 16/08/23.
+//
+
+import SwiftUI
+
+@main
+struct BallinStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
