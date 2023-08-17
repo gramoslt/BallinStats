394aaf8bf75a8eb06b6f1b3e9d61adf54537a4c1
Gerardo Ramos <gerx_37@hotmail.com>
2023-08-17 12:16:10 -0600

    Segmented Components

--- BallinStats/BallinStats/Views/TeamsTabView/TeamsTabView.swift
+++ BallinStats/BallinStats/Views/TeamsTabView/TeamsTabView.swift
@@ -9,47 +9,13 @@ import SwiftUI
 
 struct TeamsTabView: View {
     @ObservedObject var teamsTabViewModel: TeamsTabViewModel
-    let columns = [
-        GridItem(.adaptive(minimum: 150))
-    ]
 
     var body: some View {
         NavigationStack {
             ScrollView {
-                HStack {
-                    Picker("Category", selection: $teamsTabViewModel.selectedFilter){
-                        ForEach(Division.allCases) { division in
-                                Text(division.rawValue.capitalized)
-                            }
-                    }
-                    Spacer()
-                }
+                DivisionPicker(teamsTabViewModel: teamsTabViewModel)
 
-                LazyVGrid(columns: columns) {
-                    ForEach(TeamsTabViewModel.teamsMock, id: \.self) { team in
-                        VStack {
-                            Image("Heat-logo")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 100, height: 100)
-                            
-                            VStack {
-                                Text(team)
-                                    .font(.headline)
-                                    .foregroundColor(.white)
-                            }
-                            .padding(.vertical)
-                            .frame(maxWidth: .infinity)
-                            .background(.red)
-                        }
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 10)
-                                .stroke(Color.yellow)
-                        )
-                        .padding()
-                    }
-                }
+                TeamsDynamicGrid(teamsTabViewModel: teamsTabViewModel)
             }
             .navigationTitle(TabViewConstants.teamsLabel)
             .background(Color("Dark-Blue-BackgroundColor"))
