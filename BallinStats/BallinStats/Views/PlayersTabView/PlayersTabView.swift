0daef79af73e46aa49f2eda27621d2180d99df23
Gerardo Ramos <gerx_37@hotmail.com>
2023-08-17 08:36:49 -0600

    Squashed commit of the following:
    commit b537d3afee892592753e71d5cb416c4ac6536e23
    Author: Gerardo Ramos <gerx_37@hotmail.com>
    Date:   Wed Aug 16 19:29:33 2023 -0600
    
        Updated Texts
    
    commit 04c994583aad9b116661824a05b541757fcdc350
    Author: Gerardo Ramos <gerx_37@hotmail.com>
    Date:   Wed Aug 16 19:27:32 2023 -0600
    
        applied texts
    
    commit 348fc0aee6446e209c2eae0bb3e1ae5fcf5462cf
    Author: Gerardo Ramos <gerx_37@hotmail.com>
    Date:   Wed Aug 16 19:22:37 2023 -0600
    
        Updated TabView to use constants
    
    commit 28d4e7801dd2f024e21a29c8ccd719a838b19d73
    Author: Gerardo Ramos <gerx_37@hotmail.com>
    Date:   Wed Aug 16 19:22:26 2023 -0600
    
        Created Constants file
    
    commit 69ad650619da0985cede745302fcbd2e38ae1e93
    Author: Gerardo Ramos <gerx_37@hotmail.com>
    Date:   Wed Aug 16 18:44:16 2023 -0600
    
        Created TabNavigationView and blank views for it

---
+++ BallinStats/BallinStats/Views/PlayersTabView/PlayersTabView.swift
@@ -0,0 +1,20 @@
+//
+//  PlayersTabView.swift
+//  BallinStats
+//
+//  Created by Gerardo Leal on 16/08/23.
+//
+
+import SwiftUI
+
+struct PlayersTabView: View {
+    var body: some View {
+        Text("Players View")
+    }
+}
+
+struct PlayersTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayersTabView()
+    }
+}
