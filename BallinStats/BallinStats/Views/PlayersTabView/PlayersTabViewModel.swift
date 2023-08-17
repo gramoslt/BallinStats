bade09f03bf7dcc78ff0476ee1b335ed5869404d
Gerardo Ramos <gerx_37@hotmail.com>
2023-08-17 13:52:10 -0600

    Created PlayersTabViewModel

---
+++ BallinStats/BallinStats/Views/PlayersTabView/PlayersTabViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayersTabViewModel.swift
+//  BallinStats
+//
+//  Created by Gerardo Leal on 17/08/23.
+//
+
+import Foundation
+
+@MainActor class PlayersTabViewModel: ObservableObject {
+    @Published var searchText: String = ""
+//    @Published var players: [Player] = []
+}
