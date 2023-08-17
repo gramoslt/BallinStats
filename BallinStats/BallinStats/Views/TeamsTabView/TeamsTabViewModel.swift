cd8d6f8f781e364f9cc518033129fd907dcb0121
Gerardo Ramos <gerx_37@hotmail.com>
2023-08-17 11:38:51 -0600

    Added Division Enum for picker

--- BallinStats/BallinStats/Views/TeamsTabView/TeamsTabViewModel.swift
+++ BallinStats/BallinStats/Views/TeamsTabView/TeamsTabViewModel.swift
@@ -7,9 +7,23 @@
 
 import Foundation
 
+enum Division: String, CaseIterable, Identifiable {
+    var id: Self {
+      return self
+    }
+    case Atlantic
+    case Central
+    case SouthEast
+    case NorthWest
+    case Pacific
+    case SouthWest
+    case AllTeams = "All Teams"
+}
+
 @MainActor class TeamsTabViewModel: ObservableObject {
 //    @Published var teams: [TeamDetails] = []
     @Published var teams: [String] = []
+    @Published var selectedFilter: Division = .AllTeams
 
     static let teamsMock = ["Lakers", "Celtics", "Hawks"]
 }
