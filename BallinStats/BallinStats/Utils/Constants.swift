28d4e7801dd2f024e21a29c8ccd719a838b19d73
Gerardo Ramos <gerx_37@hotmail.com>
2023-08-16 19:22:26 -0600

    Created Constants file

---
+++ BallinStats/BallinStats/Utils/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  BallinStats
+//
+//  Created by Gerardo Leal on 16/08/23.
+//
+
+import Foundation
+
+enum TabViewConstants {
+    public static let teamsLabel = "Teams"
+    public static let playersLabel = "Players"
+    public static let followingLabel = "Following"
+    public static let teamsIconString = "basketball.fill"
+    public static let playersIconString = "figure.basketball"
+    public static let followingIconString = "star.following"
+}
