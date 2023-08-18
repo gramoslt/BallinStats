ecd2be8bea8d5008d67be9ef036e71a9eaa5d783
Gerardo Ramos <gerx_37@hotmail.com>
2023-08-18 13:47:13 -0600

    Designed TeamDetailsView

--- BallinStats/BallinStats/Views/TeamDetailsView/TeamDetailsView.swift
+++ BallinStats/BallinStats/Views/TeamDetailsView/TeamDetailsView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct TeamDetailsView: View {
     var team: TeamDetails
+    @StateObject var teamDetailsViewModel = TeamDetailsViewModel()
 
     var body: some View {
         NavigationStack {
@@ -20,9 +21,34 @@ struct TeamDetailsView: View {
                     Spacer()
                 }
                 .padding(.horizontal)
+                
+                Image(team.logoString)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: TeamDetailsViewConstants.logoWidth)
+                
+                HStack (alignment: .lastTextBaseline){
+                    VStack (alignment: .leading){
+                        Text(team.city)
+                            .font(.title)
+                            .bold()
+                        Text(team.conference)
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                    }
+                    Spacer()
+                    Text(team.division)
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                }
+                .padding()
+                
+                GamesPlayedGrid(teamDetailsViewModel: teamDetailsViewModel)
             }
             .navigationTitle(team.fullName)
+            .background(.customBackgroundColor)
         }
+        .preferredColorScheme(.dark)
     }
 }
 
