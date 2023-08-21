d0244fdef48728009ebc1a0928d8bf764f704a06
Gerardo Ramos <gerx_37@hotmail.com>
2023-08-21 11:58:05 -0600

    Created EndpointBuilder.swift

---
+++ BallinStats/BallinStats/Models/EndpointBuilder.swift
@@ -0,0 +1,19 @@
+//
+//  EndpointBuilder.swift
+//  BallinStats
+//
+//  Created by Gerardo Leal on 21/08/23.
+//
+
+import Foundation
+
+class EndpointBuilder {
+    private var components = URLComponents()
+
+    init(components: URLComponents = URLComponents()) {
+        self.components = components
+        self.components.scheme = "https"
+        self.components.host = "www.balldontlie.io"
+        self.components.path = "api/v1"
+    }
+}
