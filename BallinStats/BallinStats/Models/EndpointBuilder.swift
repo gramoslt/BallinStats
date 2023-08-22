4c139d295ac049ad38ea15b9630827115e0f18b5
Gerardo Ramos <gerx_37@hotmail.com>
2023-08-22 09:25:10 -0600

    Update EndpointBuilder.swift

--- BallinStats/BallinStats/Models/EndpointBuilder.swift
+++ BallinStats/BallinStats/Models/EndpointBuilder.swift
@@ -8,12 +8,12 @@
 import Foundation
 
 class EndpointBuilder {
-    private var components = URLComponents()
+    private var components: URLComponents
 
     init(components: URLComponents = URLComponents()) {
         self.components = components
         self.components.scheme = "https"
         self.components.host = "www.balldontlie.io"
-        self.components.path = "api/v1"
+        self.components.path = "/api/v1"
     }
 }
