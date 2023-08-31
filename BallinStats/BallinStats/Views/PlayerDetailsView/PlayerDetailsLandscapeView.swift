0cc3980c1da868b1f3252184e336a5ed96e45eed
Gerardo Ramos <gerx_37@hotmail.com>
2023-08-31 11:40:01 -0600

    Added PlayerDetailsLandscapeView

---
+++ BallinStats/BallinStats/Views/PlayerDetailsView/PlayerDetailsLandscapeView.swift
@@ -0,0 +1,42 @@
+//
+//  PlayerDetailsLandscapeView.swift
+//  BallinStats
+//
+//  Created by Gerardo Leal on 31/08/23.
+//
+
+import SwiftUI
+
+struct PlayerDetailsLandscapeView: View {
+    @ObservedObject var playerDetailsViewModel: PlayerDetailsViewModel
+
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                HStack {
+                    VStack {
+                        TeamLogo(logoString: playerDetailsViewModel.player.team.logoString)
+                    }
+                    VStack {
+                        PlayerPosition(playerDetailsViewModel: playerDetailsViewModel)
+                        PlayerStatsTable(playerDetailsViewModel: playerDetailsViewModel)
+                        PlayerMeasurementsTable(playerDetailsViewModel: playerDetailsViewModel)
+                    }
+                    .padding()
+                }
+                
+            
+            }
+            .frame(maxWidth: .infinity)
+            .background(playerDetailsViewModel.backgroundColor)
+            .navigationTitle(playerDetailsViewModel.player.fullName)
+        }
+    }
+}
+
+struct PlayerDetailsLandscapeView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayerDetailsLandscapeView(playerDetailsViewModel: PlayerDetailsViewModel(player: Player.mock))
+            .previewInterfaceOrientation(.landscapeLeft)
+    }
+}
