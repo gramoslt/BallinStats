8081120ecbf2fc79d6216827098d9b39aae1cabd
Gerardo Ramos <gerx_37@hotmail.com>
2023-09-04 12:03:25 -0600

    Added extension to get gray scale

--- BallinStats/BallinStats/Utils/Colors.swift
+++ BallinStats/BallinStats/Utils/Colors.swift
@@ -21,3 +21,24 @@ extension ShapeStyle where Self == Color {
         Color("RedTextColor")
     }
 }
+
+extension Color {
+    func getRGBComponents() -> (red: Double, green: Double, blue: Double)? {
+        if let uiColor = UIColor(self).cgColor.components, uiColor.count >= 3 {
+            let red = Double(uiColor[0])
+            let green = Double(uiColor[1])
+            let blue = Double(uiColor[2])
+            return (red, green, blue)
+        }
+        return nil
+    }
+
+    func getGrayScaleValue() -> Double {
+        let rgbComponents = self.getRGBComponents()
+        let red = (rgbComponents?.red ?? 0)
+        let green = (rgbComponents?.green ?? 0)
+        let blue = (rgbComponents?.blue ?? 0)
+        let grayScale = (red + green + blue) / 3
+        return grayScale
+    }
+}
